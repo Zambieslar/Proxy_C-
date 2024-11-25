03839d312f1c248bf54315e0e2da80df25033302
Zambieslar <brandonj.pershall@gmail.com>
2024-11-25 15:31:52 -0700

    Refactored state machine to improve logic and memory access

--- StateMachine.h
+++ StateMachine.h
@@ -1,84 +1,199 @@
+#include <iostream>
+#include <iterator>
+#include <ostream>
+#include <stdio.h>
 #include <string>
+#include <thread>
+#include <unistd.h>
 #include <vector>
-#include "HttpStatusCodes.h"
 
-#define NEW_LINE		0x0A
-#define CARRIAGE_RETURN 	0x0D
-#define SPACE			0x20
+#define LINE_FEED 0x0A
+#define CARRIAGE_RETURN 0x0D
+#define SPACE 0x20
 
-enum States 
-{
-	Method,
-	Path,
-	Version,
-	Host,
-	Headers,
-};
 namespace Parser {
-	class StateMachine {
+class StateMachine {
+private:
+  enum States {
+    Method,
+    Path,
+    Version,
+    Host,
+    Headers,
+    Body,
+  };
+
+  struct HttpHeader {
+    std::string Key;
+    std::string Value;
+  };
+
+  struct Request {
+    std::string Method;
+    std::string Path;
+    std::string Version;
+    std::vector<HttpHeader> Headers;
+  };
+
+  struct Machine {
+    States State = Method;
+    int index = 0;
+    std::string token;
+    HttpHeader header;
+  };
+
 public:
-	struct HttpHeader {
-		std::string Key;
-		std::string Value;
-	};
+  bool isLF(char c) {
+    if (c == LINE_FEED) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-	struct Request {
-		std::string Method;
-		std::string Path;
-		std::string Version;
-		std::string Host;
-		std::vector<HttpHeader> Headers;
-	};
+  bool isCR(char c) {
+    if (c == CARRIAGE_RETURN) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-	struct machine {
-		States State;
-		int index;
-		std::vector<char> buf;
-	};
+  bool isSP(char c) {
+    if (c == SPACE) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-	bool run(char* buf, machine machine) {
-		Request request;
-		while (true) {
-			switch (machine.State) 
-			{
-				case Method:
-					machine.buf.push_back(buf[machine.index]);
-					machine.index++;
-					if (buf[machine.index] == SPACE) {
-						request.Method = machine.buf.data();
-						machine.buf.clear();
-						machine.State = Path;
-						continue;
-					}
+  bool isHeaderDelim(char c) {
+    if (c == ':') {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-				case Path:
-					machine.buf.push_back(buf[machine.index]);
-					machine.index++;
-					if (buf[machine.index] == SPACE) {
-						request.Method = machine.buf.data();
-						machine.buf.clear();
-						machine.State = Version;
-						continue;
-					}
+  void acquireToken(char *buf, Machine *machine) {
+    // Check for delimiter. If a delimiter is reached (CR, LF, SP) then itterate
+    // the index past the delimiter in the char array.
+    while ((isSP(buf[machine->index]) || isCR(buf[machine->index]) ||
+            isLF(buf[machine->index]))) {
+      machine->index++;
+    }
+    // Capture chars while delimiter is false.
+    while (!(isSP(buf[machine->index]) || isCR(buf[machine->index]) ||
+             isLF(buf[machine->index]))) {
+      // If key-value delimiter is found then we capture they key and values in
+      // the conditional statement.
+      if (buf[machine->index] == ':') {
+        HttpHeader header;
+        machine->header.Key = machine->token;
+        machine->token.clear();
+        // Capture chars while delimiter is false.
+        while (!(isSP(buf[machine->index]) || isCR(buf[machine->index]) ||
+                 isLF(buf[machine->index]))) {
+          machine->token.push_back(buf[machine->index]);
+          machine->index++;
+        }
+        machine->header.Value = machine->token;
+        machine->token.clear();
+        if ((isCR(buf[machine->index] || isLF(buf[machine->index])))) {
+        }
+        return;
+      }
+      machine->token.push_back(buf[machine->index]);
+      machine->index++;
+    }
+  }
 
-				case Version:
-					machine.buf.push_back(buf[machine.index]);
-					machine.index++;
-					if (buf[machine.index] == NEW_LINE || CARRIAGE_RETURN) {
-						request.Method = machine.buf.data();
-						machine.buf.clear();
-						machine.State = Host;
-						continue;
-					}
+  void run(char *buf, int bytes) {
+    Request request;
+    Machine machine;
+    while (machine.index != bytes) {
 
-				case Host:
+      switch (machine.State) {
 
-				case Headers:
-					
-				default:
+      case Method:
+        acquireToken(buf, &machine);
+        request.Method = machine.token;
+        machine.State = Path;
+        machine.token.clear();
 
-			}
-		};
-	};
-	};
+      case Path:
+        acquireToken(buf, &machine);
+        request.Path = machine.token;
+        machine.State = Version;
+        machine.token.clear();
+
+      case Version:
+        acquireToken(buf, &machine);
+        request.Version = machine.token;
+        machine.State = Headers;
+        machine.token.clear();
+
+      case Headers:
+        acquireToken(buf, &machine);
+        request.Headers.push_back(machine.header);
+
+      default:
+        machine.token.clear();
+      }
+    };
+  };
 };
+} // namespace Parser
+  //      switch (machine.State) {
+  //      case Method:
+  //        while (buf[machine.index] != SPACE) {
+  //          machine.token.push_back(buf[machine.index]);
+  //          machine.index++;
+  //        }
+  //        machine.index++;
+  //        machine.State = Path;
+  //        request.Method =
+  //            std::string(machine.token.begin(), machine.token.end());
+  //        machine.token.clear();
+  //
+  //      case Path:
+  //        while (buf[machine.index] != SPACE) {
+  //          machine.token.push_back(buf[machine.index]);
+  //          machine.index++;
+  //        }
+  //        machine.index++;
+  //        machine.State = Version;
+  //        request.Path = std::string(machine.token.begin(),
+  //        machine.token.end()); machine.token.clear();
+  //
+  //      case Version:
+  //        while (buf[machine.index] != CARRIAGE_RETURN) {
+  //          machine.token.push_back(buf[machine.index]);
+  //          machine.index++;
+  //        }
+  //        while (buf[machine.index] == (CARRIAGE_RETURN | LINE_FEED)) {
+  //          machine.token.push_back(buf[machine.index]);
+  //          machine.index++;
+  //        }
+  //        request.Version =
+  //            std::string(machine.token.begin(), machine.token.end());
+  //        machine.State = Headers;
+  //        machine.token.clear();
+  //
+  //      case Headers:
+  //        while (machine.index != bytes) {
+  //          std::vector<std::string> lines;
+  //          while (buf[machine.index] != (CARRIAGE_RETURN)) {
+  //            machine.token.push_back(buf[machine.index]);
+  //            machine.index++;
+  //          }
+  //          printf("%s\n", machine.token.data());
+  //          return request;
+  //        }
+  //
+  //      default:
+  //        return request;
+  //      }
+  //    };
+  //    return request;
+  //  };
--- main.cpp
+++ main.cpp
@@ -1,44 +1,51 @@
+#include "./HttpStatusCodes.h"
+#include "./StateMachine.h"
 #include <cstdlib>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <netinet/in.h>
 #include <unistd.h>
-#include "./HttpStatusCodes.h"
 
 #define SOCKET_PORT 8080
 
 int main() {
-	char buf[1024];
-	sockaddr_in serv_addr, cli_addr;
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(SOCKET_PORT);
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	if (sockfd < 0) {
-		perror("Failed to create socket\n");
-	} else {
-		printf("Created socket\n");
-	}
-	
-	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-		perror("Failed to bind socket\n");
-	} else {
-		printf("Socket bound to 0.0.0.0:8080\n");
-	}
-	listen(sockfd, 5);
-	int clilen = sizeof(cli_addr);
+  Parser::StateMachine StateMachine;
+  char buf[1024];
+  sockaddr_in serv_addr, cli_addr;
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(SOCKET_PORT);
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  if (sockfd < 0) {
+    perror("Failed to create socket\n");
+  } else {
+    printf("Created socket\n");
+  }
 
-	int newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t *) &clilen);
+  while (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    sleep(2);
+    perror("Failed to bind socket");
+  }
+  printf("Socket bound to 0.0.0.0:8080\n");
 
-	if (newsockfd < 0) {
-		perror("Accept call failed\n");
-	} else {
-		int n = read(newsockfd, &buf, 255);
-		if (n < 0) {
-			perror("Failed to read from socket\n");
-		}
-		printf("%s", buf);
-	}
+  listen(sockfd, 5);
+  int clilen = sizeof(cli_addr);
 
+  int newsockfd =
+      accept(sockfd, (struct sockaddr *)&cli_addr, (socklen_t *)&clilen);
+
+  if (newsockfd < 0) {
+    perror("Accept call failed\n");
+  } else {
+    int n = read(newsockfd, &buf, 1023);
+
+    if (n < 0) {
+      perror("Failed to read from socket\n");
+    }
+    StateMachine.run(buf, n);
+    close(sockfd);
+    close(newsockfd);
+  }
+  return 0;
 }
